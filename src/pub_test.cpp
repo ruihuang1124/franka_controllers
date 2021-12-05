7fc33db627c47daa0d78cb86a6d9bfd2c0dd3ca9
ruihuang1124 <hr1415926>
2021-12-05 15:24:30 +0800

    add right tilt gravity direction, add rotation matrix calculation code in pub_test

--- src/pub_test.cpp
+++ src/pub_test.cpp
@@ -1,16 +1,9 @@
 #include "ros/ros.h"
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Twist.h>
+#include <eigen3/Eigen/Dense>
+#include <cmath>
 
-
-/*
-    pub person msg
-    1. include .h file
-    2. init ros node
-    3. create ros nodehandle
-    4. create pub object
-    5. write pub logict and pub msg
-*/
 
 int main(int argc, char *argv[])
 {
@@ -43,13 +36,6 @@ int main(int argc, char *argv[])
     person.pose.orientation.y = -0.5168;
     person.pose.orientation.z = -0.0198;
     person.pose.orientation.w = 0.8254;
-    //x:0.5525, y:0.4208, z:0.7215, QX:0.2263, QY:-0.5168, QZ:-0.0198, QW:0.8254
-    //x:0.6054, y:0.0177, z:1.2426, QX:0.9193, QY:-0.3630, QZ:0.1513, QW:-0.0151
-    //x:0.6605, y:0.0038, z:1.0788, QX:0.9223, QY:-0.3778, QZ:0.0800, QW:0.0160
-    //x:0.3034, y:0.0045, z:0.5813, QX:0.9138, QY:-0.3930, QZ:0.1023, QW:0.0107
-    // x:0.3077, y:0.0014, z:0.5880, QX:0.9247, QY:-0.3807, QZ:-0.0011, QW:0.0034
-    // x:0.7206, y:-0.0031, z:1.1095, QX:0.9234, QY:-0.3839, QZ:0.0002, QW:0.0010
-    //x:0.3123, y:-0.0088, z:0.5837, QX:0.9317, QY:-0.3613, QZ:-0.0311, QW:0.0196
 
     geometry_msgs::Twist mobile_move_test;
     mobile_move_test.linear.x = -0.05;
@@ -58,6 +44,83 @@ int main(int argc, char *argv[])
     mobile_move_test.angular.x = 0;
     mobile_move_test.angular.y = 0;
     mobile_move_test.angular.z = 0;
+    
+    //Code about Calculating the rotation matrix and gravity direction when tilt mounting
+    Eigen::MatrixXf rotation1(4, 4), rotation2(4, 4), rotation3(4, 4), rotation4(4, 4), rotation5(4, 4), rotationFinal(4, 4), rotation6(4, 4), rotation7(4, 4);
+    Eigen::MatrixXf vet1(4, 1), vet2(4, 1), vet3_gravity_incustomized(4, 1), vetG(4, 1), vettemp(4, 1);
+    vet1 << 0, 1, 0, 1;
+    vetG << 0, 0, -9.81, 0;
+    rotation1 << 0, 0, 1, 0,
+        0, 1, 0, 0,
+        -1, 0, 0, 0,
+        0, 0, 0, 1; //rotation around Y 90 degree;
+    rotation2 << 1, 0, 0, 0,
+        0, cos(PI / 4), -sin(PI / 4), 0,
+        0, sin(PI / 4), cos(PI / 4), 0,
+        0, 0, 0, 1; //rotation around X -45 degree;
+    rotation3 << cos(PI / 6), -sin(PI / 6), 0, 0,
+        sin(PI / 6), cos(PI / 6), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1; //rotation around Z -30 degree;
+    rotation6 << cos(-10 * PI / 180), 0, sin(-10 * PI / 180), 0,
+        0, 1, 0, 0,
+        -sin(-10 * PI / 180), 0, cos(-10 * PI / 180), 0,
+        0, 0, 0, 1; //rotation vector in original base frame, Y+ direction
+
+    rotation7 = rotation6 * rotation1 * rotation2 * rotation3;
+
+    rotation4 = rotation1 * rotation2 * rotation3;
+
+    vet2 = rotation4.inverse() * vet1;
+    vet2(3, 0) = 0;
+    vet2.normalize();
+    ROS_ERROR("tes value is:%.8f,%.8f,%.8f，%.8f", vet2(0, 0), vet2(1, 0), vet2(2, 0), vet2.norm());
+    double a = vet2(0, 0);
+    double b = vet2(1, 0);
+    double c = vet2(2, 0);
+    double xita = -10 * PI / 180;
+    ROS_ERROR("tes value is:%.8f,%.8f,%.8f，%.8f", vet2(0, 0), vet2(1, 0), vet2(2, 0), vet2.norm());
+    rotation5(0, 0) = a * a + (1 - a * a) * cos(xita);
+    rotation5(0, 1) = a * b * (1 - cos(xita)) + c * sin(xita);
+    rotation5(0, 2) = a * c * (1 - cos(xita)) - b * sin(xita);
+    rotation5(0, 3) = 0;
+    rotation5(1, 0) = a * b * (1 - cos(xita)) - c * sin(xita);
+    rotation5(1, 1) = b * b + (1 - b * b) * cos(xita);
+    rotation5(1, 2) = b * c * (1 - cos(xita)) + a * sin(xita);
+    rotation5(1, 3) = 0;
+    rotation5(2, 0) = a * c * (1 - cos(xita)) + b * sin(xita);
+    rotation5(2, 1) = b * c * (1 - cos(xita)) - a * sin(xita);
+    rotation5(2, 2) = c * c + (1 - c * c) * cos(xita);
+    rotation5(2, 3) = 0;
+    rotation5(3, 0) = 0;
+    rotation5(3, 1) = 0;
+    rotation5(3, 2) = 0;
+    rotation5(3, 3) = 1;
+
+    // rotation5(0, 0) = a * a * (1-cos(xita)) +  cos(xita);
+    // rotation5(0, 1) = a * b * (1 - cos(xita)) + c * sin(xita);
+    // rotation5(0, 2) = a * c * (1 - cos(xita)) - b * sin(xita);
+    // rotation5(0, 3) = 0;
+    // rotation5(1, 0) = a * b * (1 - cos(xita)) - c * sin(xita);
+    // rotation5(1, 1) = b * b * (1 - cos(xita)) + cos(xita);
+    // rotation5(1, 2) = b * c * (1 - cos(xita)) + a * sin(xita);
+    // rotation5(1, 3) = 0;
+    // rotation5(2, 0) = a * c * (1 - cos(xita)) + b * sin(xita);
+    // rotation5(2, 1) = b * c * (1 - cos(xita)) - a * sin(xita);
+    // rotation5(2, 2) = c * c * (1 - cos(xita)) + cos(xita);
+    // rotation5(2, 3) = 0;
+    // rotation5(3, 0) = 0;
+    // rotation5(3, 1) = 0;
+    // rotation5(3, 2) = 0;
+    // rotation5(3, 3) = 1;
+
+    rotationFinal = rotation5 * rotation4;
+    ROS_WARN_STREAM(rotationFinal);
+    vet3_gravity_incustomized = rotation7.inverse() * vetG;
+    vet3_gravity_incustomized(3, 0) = 0;
+    ROS_ERROR("The final value is:%.16f,%.16f,%.16f,%.16f", vet3_gravity_incustomized(0, 0), vet3_gravity_incustomized(1, 0), vet3_gravity_incustomized(2, 0), vet3_gravity_incustomized.norm());
+
+    //
     //set pub rate
     ros::Rate rate(6);
     //pub through loop
